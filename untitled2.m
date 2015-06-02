28260b77ee54a8d67ca94289eb0c8a31cb6b0386
Alex Liberzon <alex.liberzon@gmail.com>
2015-06-03 01:25:43 +0300

    fixed an old bug that was there due to copy/paste - the not updated interrogation window when the data is black.

--- untitled2.m
+++ untitled2.m
@@ -15,45 +15,44 @@ NfftHeight = 2*ittHeight;
 
 for m = 1:ovlapVer:verSize - ittHeight + 1 % vertically
     for k = 1:ovlapHor:horSize-ittWidth+1 % horizontally
-        if (get(hObject,'UserData') == 1)
-            a2 = a1(m:m+ittHeight-1,k:k+ittWidth-1);
-            b2 = b1(m:m+ittHeight-1,k:k+ittWidth-1);
-            
-            %                         a2 = prepfun(a2);
-            %                         b2 = prepfun(b2);
-            
-            c = cross_correlate_rect(a2,b2,NfftHeight,NfftWidth);
-            % c = cross_correlate_rect(a2,b2,Nfftx,Nffty);
-            if ~any(c(:)), % completely "black"
-                u = 0;
-                v = 0;
-                y = origin(2) + m + ittHeight/2 - 1;
-                x = origin(1) + k + ittWidth/2 -  1;
-                resind = resind + 1;
-                s2n = 0;
-                res(resind,:) = [x y u v s2n];
-                continue
-            end
-            
-            [peak1,peak2,pixi,pixj] = find_displacement_rect(c,s2ntype);
-            
-            [peakVer,peakHor,s2n] = sub_pixel_velocity_rect(c,pixi,pixj,peak1,peak2,s2nl,ittWidth,ittHeight);
-            
-            % Scale the pixel displacement to the velocity
-            u = (ittWidth-peakHor);
-            v = (ittHeight-peakVer);
-            y = origin(2) + m + ittHeight/2-1;
-            x = origin(1) + k + ittWidth/2-1;
-            
-            resind = resind + 1;
-            res(resind,:) = [x y u v s2n];
-            % quiver(x+cropvec(1),y+cropvec(2),u,v,'y');
-            if u ~= 0 || v ~= 0
-                %                             quiver(x,y,u,v,5,'y','Linewidth',1);
-                %                             drawnow;
-                plotarrow(x,y,u,v,'g',10);
-                % drawnow
-            end
+        % if Stop button pressed:
+        if (get(handles.pushbutton_start,'UserData') == 0)
+            return;
+        end
+        
+        a2 = a1(m:m+ittHeight-1,k:k+ittWidth-1);
+        b2 = b1(m:m+ittHeight-1,k:k+ittWidth-1);
+        
+        
+        c = cross_correlate_rect(a2,b2,NfftHeight,NfftWidth);
+        % c = cross_correlate_rect(a2,b2,Nfftx,Nffty);
+        if ~any(c(:)), % completely "black"
+            u = 0;
+            v = 0;
+            y = origin(1) + m + ittHeight/2 - 1;
+            x = origin(2) + k + ittWidth/2 -  1;
+            continue
+        end
+        
+        [peak1,peak2,pixi,pixj] = find_displacement_rect(c,s2ntype);
+        
+        [peakVer,peakHor,s2n] = sub_pixel_velocity_rect(c,pixi,pixj,peak1,peak2,s2nl,ittWidth,ittHeight);
+        
+        % Scale the pixel displacement to the velocity
+        u = (ittWidth-peakHor);
+        v = (ittHeight-peakVer);
+        y = origin(2) + m + ittHeight/2-1;
+        x = origin(1) + k + ittWidth/2-1;
+        
+        resind = resind + 1;
+        res(resind,:) = [x y u v s2n];
+        % quiver(x+cropvec(1),y+cropvec(2),u,v,'y');
+        if u ~= 0 || v ~= 0
+            %                             quiver(x,y,u,v,5,'y','Linewidth',1);
+            %                             drawnow;
+            plotarrow(x,y,u,v,'g',10);
+            % draw_arrow([x,y],[x+u,y+v],20)
+            % drawnow
         end
     end
 end
@@ -62,8 +61,6 @@ end
 
 % NO_FILT_RES will be stored in '.._noflt.txt' file at the end of program
 no_filt_res = res;
-
-
 
 % Reshape U and V matrices in two-dimensional grid and produce
 % velocity vector in U + i*V form (real and imaginary parts):
@@ -81,7 +78,6 @@ u = real(vector);
 v = imag(vector);
 
 % Adaptive Local Median filtering
-
 kernel = [-1 -1 -1; -1 8 -1; -1 -1 -1];
 tmpv = abs(conv2(v,kernel,'same'));
 tmpu = abs(conv2(u,kernel,'same'));
